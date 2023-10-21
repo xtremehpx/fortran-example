978dd4a4de2a153dd6f40436ea1d79e2504fc0d4
Paul <hui.p.xiong@gmail.com>
2023-10-21 07:36:14 -0700

    add wait for command line

--- hello-world/hello.f90
+++ hello-world/hello.f90
@@ -1,4 +1,6 @@
 program hello
   ! This is a comment line; it is ignored by the compiler
   print *, 'Fotran - Hello, World!'
+  print *, 'Press Enter to exit the program'
+  read(*,*)
 end program hello
